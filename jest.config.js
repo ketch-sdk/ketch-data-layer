97f0512a8c71d60e415b1e9bb08081f96aafb9eb
Seth Yates <29878+sethyates@users.noreply.github.com>
2023-01-03 19:52:26 -0800

    fix: remove ncc

--- jest.config.js
+++ jest.config.js
@@ -151,10 +151,10 @@ module.exports = {
   // timers: "real",
 
   // A map from regular expressions to paths to transformers
-  // transform: null,
+  // transform: {},
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  transformIgnorePatterns: ['node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)'],
+  // transformIgnorePatterns: ['node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)'],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
