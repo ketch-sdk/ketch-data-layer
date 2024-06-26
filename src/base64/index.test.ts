059cb0dc9a4470f808d6aae293185c5c1ddd7cdf
Sam Alexander <sam@ketch.com>
2024-06-26 12:26:50 -0700

    feat: encode

---
+++ src/base64/index.test.ts
@@ -0,0 +1,35 @@
+import { encoding } from './index'
+
+describe('base64', () => {
+  describe('encoding', () => {
+    it('decodes the base64 and url encoded string', async () => {
+      const input = ['dGVzdEBlbWFpbC5jb20%3D']
+      const actual = await encoding(input)
+      expect(actual).toStrictEqual(['test@email.com'])
+    })
+
+    it('decodes the base64 string without url encoding', async () => {
+      const input = ['dGVzdEBlbWFpbC5jb20=']
+      const actual = await encoding(input)
+      expect(actual).toStrictEqual(['test@email.com'])
+    })
+
+    it('decodes the base64 encoded json', async () => {
+      const input = ['eyJlbWFpbCI6ICJ0ZXN0QGVtYWlsLmNvbSIsICJuYW1lIjogInRlc3QifQ%3D%3D']
+      const actual = await encoding(input)
+      expect(actual).toStrictEqual(['{"email": "test@email.com", "name": "test"}'])
+    })
+
+    it('does not affect json', async () => {
+      const input = [{foo: 'val1', bar: 2}]
+      const actual = await encoding(input)
+      expect(actual).toStrictEqual([{foo: 'val1', bar: 2}])
+    })
+
+    it('handles list of inputs', async () => {
+      const input = ['dGVzdEBlbWFpbC5jb20%3D', {foo: 'val1', bar: 2}]
+      const actual = await encoding(input)
+      expect(actual).toStrictEqual(['test@email.com', {foo: 'val1', bar: 2}])
+    })
+  })
+})
---
+++ src/noop/index.ts
@@ -0,0 +1,3 @@
+import encoding from './encoding'
+
+export { encoding }
